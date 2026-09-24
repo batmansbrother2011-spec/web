f17f5b73fe61d6bd111ff0a0980dd51e82b1b05f
batmansbrother2011-spec <batmansbrother2011@gmail.com>
2026-09-24 14:43:34 -0400

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -44,7 +44,7 @@ const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
     "no-useless-escape": "off",
   },
 }, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts", "examples/**", "skills"]
+  ignores: ["node_modules/**", ".next/**", ".open-next/**", "out/**", "build/**", "next-env.d.ts", "examples/**", "skills", "public/games/**"]
 }];
 
 export default eslintConfig;
